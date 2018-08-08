6e1e7c4f54e5d90d6bde2d2ee6a796e242b0aea1
chupkar <42171245+chupkar@users.noreply.github.com>
2018-08-08 18:22:28 +0500

    dd
    dd

---
+++ WebApplication29/Controllers/mailController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication29.Controllers
+{
+    public class mailController : Controller
+    {
+        // GET: mail
+
+       
+        public ActionResult Index()
+        {
+          
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string to, string from, string pass,string sub, string msg)
+        {
+            try
+            {
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = true;
+
+                MailMessage mail = new MailMessage(from,to);
+                mail.Subject = sub;
+                mail.IsBodyHtml = true;
+                mail.Body = "<html><p><b>Hello </b></p></html>";
+
+                smtp.Credentials = new NetworkCredential(from, pass);
+                smtp.Send(mail);
+                ViewBag.msg = "mail sent!";
+
+
+
+
+
+            }
+            catch(Exception ex)
+            {
+
+                ViewBag.msg = ex.Message;
+            }
+
+
+            return View();
+        }
+    }
+}
